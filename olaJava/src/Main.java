48ec840218a1b4b3b52747bffc84b6f075e45e4c
emartiinsx <enricolmpereira@gmail.com>
2026-07-28 14:27:00 -0300

    Add files via upload

---
+++ olaJava/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Ola Java");
+        int x = 10;
+        int y  =10;
+        int z = 5;
+
+        System.out.println(y + z);
+    }
+
+}
